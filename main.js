ce476e6e16378f3ca2458dc2b217e4baf109fb25
Ernesto <egonzalez442@gmail.com>
2022-12-01 11:09:29 -0500

    added imag

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ const body = document.querySelector('body div');
     // white div w/ message and question want to play that sends to snake game
     const newDiv = document.createElement('div');
     newDiv.style.width = '100%';
-    newDiv.style.height = '550px';
-    newDiv.style.border = '4px double darkblue';
-    newDiv.style.position = 'absolute';
+    newDiv.style.height = '820px';
+    newDiv.style.border = '6px double darkblue';
+    newDiv.style.borderRadius = '25px'
+    newDiv.style.position = 'fixed';
     newDiv.style.backgroundColor = 'black';
     newDiv.style.zIndex = '9999';
     newDiv.style.marginTop = '100px';
@@ -27,7 +28,7 @@ const body = document.querySelector('body div');
 
     const playBtn = document.createElement('button');
     playBtn.classList.add('btn');
-    playBtn.setAttribute('id', 'playBtn')
+    playBtn.setAttribute('id', 'playBtn');
     playBtn.innerText = "Yes, I'll play";
     playBtn.style.padding = '5px 0px';
     playBtn.style.width = "180px";
@@ -37,7 +38,7 @@ const body = document.querySelector('body div');
 
     const noPlayBtn = document.createElement('button');
     noPlayBtn.setAttribute('id', 'noPlayBtn');
-    noPlayBtn.classList.add('btn')
+    noPlayBtn.classList.add('btn');
     noPlayBtn.innerText = "Nah, I'm good.";
     noPlayBtn.style.padding = '5px 0px';
     noPlayBtn.style.width = "180px";
@@ -46,9 +47,9 @@ const body = document.querySelector('body div');
     noPlayBtn.style.color = 'darkblue';
     
     const pacman = document.createElement('img');
-    const imgURL = chrome.runtime.getURL("./pac-man.gif")
+    const imgURL = chrome.runtime.getURL("pac-man.gif")
     pacman.src = imgURL;
-
+    
     btnContainer.append(noPlayBtn);
     btnContainer.append(playBtn);
     newDiv.append(btnContainer);
@@ -61,22 +62,35 @@ noPlayBtn.addEventListener('click', function() {
 
 playBtn.addEventListener('click', function() {
     location.href = 'https://dancing-tapioca-e617ee.netlify.app/'
-})
+});
 
 noPlayBtn.addEventListener('mouseover', function() {    
 	if (noPlayBtn.style.transform === 'translateX(-200px)')
         noPlayBtn.style.transform = 'translateX(0px)';
     else noPlayBtn.style.transform = 'translateX(-200px)';
-})
+});
 
 playBtn.addEventListener('mouseover', function() {
     playBtn.style.backgroundColor = 'darkblue';
     playBtn.style.color = 'yellow';
     playBtn.style.cursor = 'pointer';
-})
+});
 
 playBtn.addEventListener("mouseout", function () {
   playBtn.style.backgroundColor = "yellow";
   playBtn.style.color = "darkblue";
   playBtn.style.cursor = 'default';
 });
+
+// function movePacman(x = 0) {
+//     if(x < 300) {
+//         x += 50;
+//         pacman.style.transform = `translateX(${x}px)`;
+//     } else {
+//         x -= 50;
+//         pacman.style.transform = `translateX(${x}px)`;
+//     }
+//     setTimeout(movePacman(x), 250);
+// }
+
+// setTimeout(movePacman, 250);
